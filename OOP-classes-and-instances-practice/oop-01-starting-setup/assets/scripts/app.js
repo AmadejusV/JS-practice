6473420baea6d11376d22c57f39e6051800abfde
Amadejus Valionis <amadeus444@gmail.com>
2021-04-28 14:38:01 +0300

    inheritance reusability super and static

--- OOP-classes-and-instances-practice/oop-01-starting-setup/assets/scripts/app.js
+++ OOP-classes-and-instances-practice/oop-01-starting-setup/assets/scripts/app.js
@@ -1,10 +1,9 @@
 //class is a blueprint for an object
 class Product {
-
-//   title = "DEFAULT";
-//   imageUrl;
-//   description;
-//   price;
+  //   title = "DEFAULT";
+  //   imageUrl;
+  //   description;
+  //   price;
 
   constructor(titleArg, imageArg, descriptionArg, priceArg) {
     this.title = titleArg;
@@ -14,10 +13,8 @@ class Product {
   }
 }
 
-
-
-class ElementAttribute{
-  constructor(attrName, attrValue){
+class ElementAttribute {
+  constructor(attrName, attrValue) {
     this.name = attrName;
     this.value = attrValue;
   }
@@ -25,17 +22,22 @@ class ElementAttribute{
 
 // a class for inheritance practice
 class Component {
-  constructor(renderHookId){
-    this.hook=renderHookId;
-  }
+  constructor(renderHookId, shouldRender = true) {
+    this.hook = renderHookId;
 
-  createElement(tag, cssClasses, attributes){
+    if (shouldRender) {
+      this.render();
+    }
+  }
+  render() {}
+
+  createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
-    if(cssClasses){
+    if (cssClasses) {
       rootElement.className = cssClasses;
     }
-    if(attributes && attributes.length > 0){
-      for(const attribute of attributes){
+    if (attributes && attributes.length > 0) {
+      for (const attribute of attributes) {
         rootElement.setAttribute(attribute.name, attribute.value);
       }
     }
@@ -45,20 +47,74 @@ class Component {
 }
 
 
+class ShoppingCart extends Component {
+  shoppingList = [];
 
-class ProductItem extends Component{
-    constructor(productObj, renderHookId){
-      super(renderHookId)
-        this.product = productObj;
+  set cartItems(value) {
+    this.shoppingList = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2> `;
+  }
+
+  get totalAmount() {
+    if(!this.shoppingList){
+      return 0;
     }
+    const sum = this.shoppingList.reduce((prevValue, currentItem) => 
+      prevValue + currentItem.price
+    , 0);
+    return sum;
+  }
 
-    addToCart(){
-      App.addProductToCart(this.product);
-    }
+  constructor(renderHookId) {
+    //super method calls the constructor of the parent class, passes it's argument to parent constructor parameters as argument
+    super(renderHookId);
+    // this.render();    will use overriding to call it from the component class
+  }
 
-    renderProduct(){
-        const productItem = this.createElement("li", "product-item");
-        productItem.innerHTML = `
+  addProduct(productObj) {
+    const updatedItems = [...this.shoppingList];
+    updatedItems.push(productObj);
+    this.cartItems = updatedItems;
+  }
+
+  orderProducts(){
+    console.log("Ordering products...");
+    console.log(this.shoppingList);
+  }
+
+  render() {
+    const cartElement = this.createRootElement("section", "cart");
+    cartElement.innerHTML = `
+      <h2>Total: \$${this.totalAmount}</h2>
+      <button>Order now!</button>
+    `;
+    const orderButton = cartElement.querySelector("button");
+    // orderButton.addEventListener("click", this.orderProducts.bind(this));
+    orderButton.addEventListener("click", ()=>this.orderProducts());
+    this.totalOutput = cartElement.querySelector("h2");
+  }
+}
+
+
+class ProductItem extends Component {
+  constructor(productObj, renderHookId) {
+    //not allowing to render until product is set
+    super(renderHookId, false);
+    //setting product
+    this.product = productObj;
+    this.render();
+    // this.render();   will use overriding to call it from the component class
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const productItem = this.createRootElement("li", "product-item");
+    productItem.innerHTML = `
                   <div>
                       <img src="${this.product.imageUrl}" alt="${this.product.title}">
                       <div class="product-item__content">
@@ -69,53 +125,28 @@ class ProductItem extends Component{
                       </div>
                   </div>
               `;
-              const addToCartButton = productItem.querySelector('button');
-              //binding 'this' to ProductItem object 'this'
-              addToCartButton.addEventListener('click', this.addToCart.bind(this));
-    }
-}
-
-
-
-
-class ShoppingCart extends Component{
-  shoppingList=[];
-
-  set cartItems(value){
-    this.shoppingList = value;
-    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2> `;
-  }
-
-  get totalAmount(){
-    const sum = this.shoppingList.reduce((prevValue, currentItem)=>{return prevValue+currentItem.price}, 0);
-    return sum;
-  }
-
-  constructor(renderHookId){
-    //super method calls the constructor of the parent class, passes it's argument to parent constructor parameters as argument
-    super(renderHookId);
-  }
-  
-  addProduct(productObj){
-    const updatedItems = [...this.shoppingList];
-    updatedItems.push(productObj);
-    this.cartItems=updatedItems;
-  }
-
-  renderCart(){
-    const cartElement = this.createElement('section', 'cart');
-    cartElement.innerHTML=`
-      <h2>Total: \$${this.totalAmount}</h2>
-      <button>Order now!</button>
-    `;
-    this.totalOutput = cartElement.querySelector('h2');
+    const addToCartButton = productItem.querySelector("button");
+    //binding 'this' to ProductItem object 'this'
+    addToCartButton.addEventListener("click", this.addToCart.bind(this));
   }
 }
+
+
 
 //everything related to productList goes in it
 class ProductList extends Component {
-    products = [
-        // a new object initiation
+  products = [];
+
+  //fields are magically added during object initiation via constructor, description unnecessary
+  constructor(renderHookId) {
+    super(renderHookId);
+    this.fetchProducts();
+    // this.render();
+  }
+
+  fetchProducts() {
+    this.products = [
+      // a new object initiation
       new Product(
         "A pillow",
         "https://cdn.shopify.com/s/files/1/2509/4402/products/11-2019_S4A_Supreme_Pillow1_Standard_PDP_Retouched_300dpi_RGB_2x3_M.jpg?v=1573835697",
@@ -129,48 +160,53 @@ class ProductList extends Component {
         89.99
       ),
     ];
+    //render products after they are fetched
+    this.renderProducts();
+  }
 
-    //fields are magically added during object initiation via constructor, description unnecessary
-    constructor(renderHookId){
-      super(renderHookId);
-    };
+  renderProducts() {
+    for (const product of this.products) {
+      //giving it an object to be used for new li element creation and an 'id' to select an element to which to append it
+      new ProductItem(product, "prod-list");
+    }
+  }
 
-    //method for rendering products
-    renderProductList() {
-        this.createElement("ul", "product-list", [new ElementAttribute("id", "prod-list")]);
-
-        for (const product of this.products) {
-            const productItem = new ProductItem(product, "prod-list");
-            productItem.renderProduct();
-        }
-      }
-}
-
-
-class Shop{
-  renderShop(){
-    //setting shoppingCart as property of Shop, passing it a hookId (to find an element to which to append it)
-    this.shoppingCart = new ShoppingCart('app');
-    this.shoppingCart.renderCart();
-    const productList = new ProductList('app');
-    productList.renderProductList();
+  //method for rendering products
+  render() {
+    this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
+    ]);
+    //render products only if it's not empty
+    if (this.products && this.products.length > 0) {
+      this.renderProducts();
+    }
   }
 }
 
+class Shop extends Component {
+  constructor() {
+    super();
+    // this.renderShop(); will use overriding to call it from the component class
+  }
 
+  render() {
+    //setting shoppingCart as property of Shop, passing it a hookId (to find an element to which to append it)
+    this.shoppingCart = new ShoppingCart("app");
+    new ProductList("app");
+  }
+}
 
 class App {
   static cart;
 
-  static init(){
+  static init() {
     const shop = new Shop();
-    shop.renderShop();
     //setting App cart property as reference to shop.shoppingCart to have a global cart available for adding products
     this.cart = shop.shoppingCart;
   }
 
   //static methods can only be called on a class, not an object
-  static addProductToCart(product){
+  static addProductToCart(product) {
     this.cart.addProduct(product);
   }
 }
